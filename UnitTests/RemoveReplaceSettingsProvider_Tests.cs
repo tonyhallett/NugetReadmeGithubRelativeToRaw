d552c93e54bea1873ff0ff55a176152ad6366046
Tony Hallett <tonyhallett74@gmail.com>
2025-10-02 09:34:56 +0100

    backup

--- UnitTests/RemoveReplaceSettingsProvider_Tests.cs
+++ UnitTests/RemoveReplaceSettingsProvider_Tests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
+﻿using Microsoft.Build.Utilities;
 using Moq;
 using NugetReadmeGithubRelativeToRaw;
 using NugetReadmeGithubRelativeToRaw.MSBuildHelpers;
@@ -32,9 +31,13 @@ namespace UnitTests
         [Test]
         public void Should_Provide_Null_When_Not_Specified()
         {
-            var removeReplaceSettings = _removeReplaceSettingsProvider.Provide(null, null).Settings;
+            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide(null, null);
 
-            Assert.That(removeReplaceSettings, Is.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(removeReplaceSettingsResult.Errors, Is.Empty);
+                Assert.That(removeReplaceSettingsResult.Settings, Is.Null);
+            });
         }
 
         [Test]
@@ -43,10 +46,12 @@ namespace UnitTests
             string removeCommentIdentifiersMsBuild = "frommsbuild";
             _mockRemoveCommentsIdentifiers.Setup(parser => parser.Parse(removeCommentIdentifiersMsBuild, It.IsAny<IAddError>()))
                 .Returns(new RemoveCommentIdentifiers("start", "end"));
-            var removeCommentIdentifiers = _removeReplaceSettingsProvider.Provide(null, removeCommentIdentifiersMsBuild).Settings!.RemoveCommentIdentifiers;
+            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide(null, removeCommentIdentifiersMsBuild);
             
             Assert.Multiple(() =>
             {
+                Assert.That(removeReplaceSettingsResult.Errors, Is.Empty);
+                var removeCommentIdentifiers = removeReplaceSettingsResult.Settings!.RemoveCommentIdentifiers;
                 Assert.That(removeCommentIdentifiers!.Start, Is.EqualTo("start"));
                 Assert.That(removeCommentIdentifiers!.End, Is.EqualTo("end"));
             });
@@ -70,10 +75,10 @@ namespace UnitTests
         [Test]
         public void Should_Have_Missing_Metadata_Name_Errors_For_All_Items_And_Null_Settings()
         {
-            var missingMetadataTaskItem1 = SetUpItemWithMissingMetadata("missing1ItemSpec","missing1");
-            var missingMetadataTaskItem2 = SetUpItemWithMissingMetadata("missing2ItemSpec", "missing2");
+            var metadataItem1 = SetUpItemWithMissingMetadata("missing1ItemSpec","missing1");
+            var metadataItem2 = SetUpItemWithMissingMetadata("missing2ItemSpec", "missing2");
 
-            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide([missingMetadataTaskItem1, missingMetadataTaskItem2], null);
+            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide([metadataItem1.TaskItem, metadataItem2.TaskItem], null);
 
             Assert.Multiple(() =>
             {
@@ -84,16 +89,6 @@ namespace UnitTests
                 Assert.That(removeReplaceSettingsResult.Settings, Is.Null);
 
             });
-
-            ITaskItem SetUpItemWithMissingMetadata(string itemSpec, string missingMetadataName)
-            {
-                var missingMetadataTaskItem = new TaskItem(itemSpec);
-                var missingMetadata = new RemoveReplaceMetadata();
-                missingMetadata.AddMissingMetadataName(missingMetadataName);
-                _mockMSBuildMetadataProvider.Setup(msBuildMetadataProvider => msBuildMetadataProvider.GetCustomMetadata<RemoveReplaceMetadata>(missingMetadataTaskItem))
-                    .Returns(missingMetadata);
-                return missingMetadataTaskItem;
-            }
         }
 
         [Test]
@@ -114,6 +109,71 @@ namespace UnitTests
             });
         }
 
-        // Should_Get_Should_Have_RemovalOrReplacementProvider_Errors_But_Not_For_Items_With_Metadata_Name_Errors()
+        [Test]
+        public void Should_Have_RemovalOrReplacement_From_Metadata()
+        {
+            var metadataItem1 = SetUpItemWithMissingMetadata("item1");
+            var metadataItem2 = SetUpItemWithMissingMetadata("item2");
+
+            var removalOrReplacement1 = new RemovalOrReplacement(CommentOrRegex.Comment, "start","end","replacement");
+            var removalOrReplacement2 = new RemovalOrReplacement(CommentOrRegex.Regex, "start2", "end2", "replacemnet2");
+            
+            SetupRemovalOrReplacementProvider(removalOrReplacement1, metadataItem1);
+            SetupRemovalOrReplacementProvider(removalOrReplacement2, metadataItem2);
+
+            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide([metadataItem1.TaskItem, metadataItem2.TaskItem], null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(removeReplaceSettingsResult.Errors, Is.Empty);
+                var removalsOrReplacements = removeReplaceSettingsResult.Settings!.RemovalsOrReplacements;
+                Assert.That(removalsOrReplacements, Has.Count.EqualTo(2));
+                Assert.That(removalsOrReplacements[0], Is.SameAs(removalOrReplacement1));
+                Assert.That(removalsOrReplacements[1], Is.SameAs(removalOrReplacement2));
+            });
+
+            void SetupRemovalOrReplacementProvider(RemovalOrReplacement removalOrReplacement, MetadataItem matchingMetadataItem)
+            {
+                _mockRemovalOrReplacementProvider.Setup(removalOrReplacementProvider => removalOrReplacementProvider.Provide(It.Is<MetadataItem>(mi => mi.Metadata == matchingMetadataItem.Metadata && mi.TaskItem == matchingMetadataItem.TaskItem), It.IsAny<IAddError>()))
+                    .Returns(removalOrReplacement);
+            }
+        }
+
+        private MetadataItem SetUpItemWithMissingMetadata(string itemSpec, string? missingMetadataName = null)
+        {
+            var taskItem = new TaskItem(itemSpec);
+            var metadata = new RemoveReplaceMetadata();
+            if (missingMetadataName != null)
+            {
+                metadata.AddMissingMetadataName(missingMetadataName);
+            }
+            _mockMSBuildMetadataProvider.Setup(msBuildMetadataProvider => msBuildMetadataProvider.GetCustomMetadata<RemoveReplaceMetadata>(taskItem))
+                .Returns(metadata);
+            return new MetadataItem(metadata, taskItem);
+        }
+
+        [Test]
+        public void Should_Get_RemovalOrReplacementProvider_Errors_But_Not_For_Items_With_Metadata_Name_Errors()
+        {
+            var metadataItem1 = SetUpItemWithMissingMetadata("missing1ItemSpec", "missing1");
+            var metadataItem2 = SetUpItemWithMissingMetadata("okItemSpec");
+            var setup = _mockRemovalOrReplacementProvider.Setup(removalOrReplacementProvider => removalOrReplacementProvider.Provide(It.Is<MetadataItem>(mi => mi.Metadata == metadataItem2.Metadata && mi.TaskItem == metadataItem2.TaskItem), It.IsAny<IAddError>()));
+            setup.Verifiable();
+            setup.Callback<MetadataItem, IAddError>((_, addError) => addError.AddError("error"));
+
+            var removeReplaceSettingsResult = _removeReplaceSettingsProvider.Provide([metadataItem1.TaskItem, metadataItem2.TaskItem], null);
+
+            _mockRemovalOrReplacementProvider.Verify();
+            _mockRemovalOrReplacementProvider.VerifyNoOtherCalls();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(removeReplaceSettingsResult.Errors, Has.Count.EqualTo(2));
+                Assert.That(removeReplaceSettingsResult.Errors[0], Is.EqualTo("missing1missing1ItemSpec"));
+                Assert.That(removeReplaceSettingsResult.Errors[1], Is.EqualTo("error"));
+                Assert.That(removeReplaceSettingsResult.Settings, Is.Null);
+            });
+            
+        }
     }
 }
